f11dec701e83f2d6786cca278caf7acdf722471a
Evgeniya Naumova <jainnaumova@gmail.com>
2019-01-17 12:33:19 -0500

    second

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const layout = require("./views/layout");
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
@@ -11,12 +11,11 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log("Hello world");
+  res.send(layout("Hello world"));
 });
-
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  console.log(`app listening in ${PORT}`)
-})
+  console.log(`app listening in ${PORT}`);
+});
